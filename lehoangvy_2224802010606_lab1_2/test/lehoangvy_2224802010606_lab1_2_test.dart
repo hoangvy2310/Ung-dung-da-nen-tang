fb2f6cce5a1c28309cb5b261852ac72529833727
hoangvy2310 <lehoangvy.010192@gmail.com>
2026-04-02 22:58:47 +0700

    first commit

---
+++ lehoangvy_2224802010606_lab1_2/test/lehoangvy_2224802010606_lab1_2_test.dart
@@ -0,0 +1,8 @@
+import 'package:lehoangvy_2224802010606_lab1_2/lehoangvy_2224802010606_lab1_2.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
